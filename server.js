7e9f7619cd52d9cbcda13ca6baab206d81b5238c
UTCC Lab <utcc>
2026-03-14 10:52:39 +0700

    [:tada:] init repo

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http')
+// ---------------------------------------------------------------
+const PORT = 9888
+// ---------------------------------------------------------------
+function onClientRequest(req,resp)
+{
+    resp.writeHead(200, { 'Content-Type' : 'application/json' })
+
+    resp.write(JSON.stringify( { message: 'Hello Vercel class' } ))
+
+    resp.end()
+}
+// ---------------------------------------------------------------
+const server = http.createServer( onClientRequest )
+server.listen(PORT)
+console.log('running on '+PORT)
