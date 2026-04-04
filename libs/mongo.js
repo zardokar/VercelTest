c9e6807b4c84aeafc9900ac0d6ca5a2b8233824c
EqrthX <nontprawitchkung@gmail.com>
2026-04-04 16:33:43 +0700

    up mongo

---
+++ libs/mongo.js
@@ -0,0 +1,44 @@
+const { MongoClient }   = require('mongodb')
+
+const   db_protocol     = `mongodb+srv://`,
+        db_path         = ``,
+        db_host         = `cluster0.eg5kxia.mongodb.net`,  
+        db_port         = ``,    
+        db_url          = db_protocol+db_host+db_port+db_path
+
+let     authuser        = {
+                            username: `admin_db`, 
+                            password: `I8YtZzCQs7VuyNuI`
+                        }
+
+let     options         = {            
+                                auth: authuser ,
+                                authMechanism: `SCRAM-SHA-1`
+                        }
+
+async function runMongo()
+{
+    const dbconn    = await MongoClient.connect(db_url, options);
+    const db        = dbconn.db('testDB')
+    
+    console.log('Connected to MongoDB')
+
+    const collection = db.collection('testdoc')
+
+    const insertResult = await collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }])
+    console.log( insertResult )
+
+    const findResult = await collection.find({})
+
+    for await (const doc of findResult) {
+        console.log(doc);
+    }
+
+    await dbconn.close()
+}
+
+module.exports = {
+                    runMongoTest : runMongo
+}
+
+
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const http = require('http')
+
 const api_gacha = require('./api/gacha')
+const mongo     = require('./libs/mongo')
 // ---------------------------------------------------------------
 const PORT = process.env.PORT || 9888
 // ---------------------------------------------------------------
@@ -11,6 +13,8 @@ function onClientRequest(req,resp)
 
     if(req.method === 'POST' && pathname === '/api/gacha/open'){
         api_gacha.onRequestGacha(resp)
+    }else if(req.method === 'GET' && pathname === '/api/mongo/test' ){
+        mongo.runMongoTest()
     }else{
         resp.write(JSON.stringify( { message: 'Hello Vercel class' } ))
     }
